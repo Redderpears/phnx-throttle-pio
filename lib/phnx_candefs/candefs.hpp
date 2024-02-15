169218cee426d1a457c8863b1192b5c7d74563d8
Andrew Ealovega <Andrew@Ealovega.dev>
2024-02-15 17:01:52 -0500

    Add steering

---
+++ lib/phnx_candefs/candefs.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "cstdint"
+
+enum CanID : uint32_t {
+    SetSpeed = 0x0000006,
+    SetAngle = 0x0000004
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,20 @@
 #include "ACAN_STM32.h"
 #include "Arduino.h"
+#include "candefs.hpp"
+#include "utils.hpp"
 
-constexpr auto dac_pin = PA4;
+// DAC output to ESC
+constexpr auto THROTTLE_PIN = PA4;
+// PWM output to steering servo
+constexpr auto STEERING_PIN = PA7;
+// Dac bit resolution
+constexpr auto DAC_BITS = 12;
+// Dac resolution
+constexpr auto DAC_RES = 1 << DAC_BITS;
+// Max steering angle of the kart, in degrees, symmetric
+constexpr float MAX_STEERING = 24.0; // TODO
 
-static void can_rcv(const CANMessage &inMessage) {
+static void throttle_rcv(const CANMessage &inMessage) {
     // Blink the LED when we receive a CAN message
     digitalToggle(LED_BUILTIN);
 
@@ -13,35 +24,56 @@ static void can_rcv(const CANMessage &inMessage) {
 
     /*
     ESC uses differential voltage, so we need to invert our voltage.
-    4092 is 3.1V, and the ESCs lowest value that will still move
+    4096 is 3.1V, and the ESCs lowest value that will still move
     with no load is 3.8, so this should match roughly to the
     actual full range the ESC can be set to.
     */
-    auto out_val = (uint16_t) (4092.0 - (percent / 100.0) * 4092.0);
-    analogWrite(dac_pin, out_val);
+    auto out_val = (uint16_t) (DAC_RES - (percent / 100.0) * DAC_RES);
+    analogWrite(THROTTLE_PIN, out_val);
+}
+
+static void steering_rcv(const CANMessage &inMessage) {
+    // Blink the LED when we receive a CAN message
+    digitalToggle(LED_BUILTIN);
+
+    // Left positive angle in degrees
+    float angle = inMessage.dataFloat[0];
+
+    Serial.printf("Received message, setting steering to %f %\n", angle);
+
+    /*
+     * We assume the steering motor firmware has configured our max angle as full/0 duty cycle, so we map the range of
+     * our steering angles from positive to negative linearly, inverting because left is positive.
+     */
+    float angle_as_bits = mapfloat(-angle, -MAX_STEERING, MAX_STEERING, 0, DAC_RES);
+
+    analogWrite(STEERING_PIN, uint16_t(angle_as_bits));
 }
 
 void setup() {
     // Pin setup
     pinMode(LED_BUILTIN, OUTPUT);
-    pinMode(dac_pin, OUTPUT);
+    pinMode(THROTTLE_PIN, OUTPUT);
 
     Serial.begin(9600);
 
     // Our bus is 512k baud
     ACAN_STM32_Settings sett{500'000};
     ACAN_STM32::Filters filters;
-    // Only accept Set Speed messages
-    filters.addExtendedMask(0x0000006, 0x0000006, ACAN_STM32::DATA, can_rcv, ACAN_STM32::FIFO0);
+
+    // Throttle control messages
+    filters.addExtendedMask(CanID::SetSpeed, CanID::SetSpeed, ACAN_STM32::DATA, throttle_rcv, ACAN_STM32::FIFO0);
+    // Steering control messages
+    filters.addExtendedMask(CanID::SetAngle, CanID::SetAngle, ACAN_STM32::DATA, steering_rcv, ACAN_STM32::FIFO0);
 
     if (can.begin(sett, filters)) {
         Serial.println("Can OK");
     }
 
-    // Set our DAC to 12-bit resolution 0-4092
-    analogWriteResolution(12);
+    // Set our outputs to 12-bit resolution 0-4096
+    analogWriteResolution(DAC_BITS);
 }
 
 void loop() {
-    can.dispatchReceivedMessage0();
+    can.dispatchReceivedMessage();
 }
